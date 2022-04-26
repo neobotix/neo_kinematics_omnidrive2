1a14810587c2fc652de486a81bef8931ebd2bd5d
Pradheep-office <padhupradheep@gmail.com>
2022-04-26 14:43:40 +0200

    satisfying lint

--- include/VelocitySolver.h
+++ include/VelocitySolver.h
@@ -36,10 +36,10 @@
 #define VELOCITYSOLVER_H_
 
 #include "OmniWheel.h"
-
 #include <neo_common2/MatrixX.h>
-
 #include <math.h>
+#include <vector>
+
 #include <stdexcept>
 
 
@@ -51,65 +51,65 @@
  */
 class VelocitySolver {
 public:
-	double R_norm = 0;				// solution error
-	double move_vel_x = 0;			// solution [m/s]
-	double move_vel_y = 0;			// solution [m/s]
-	double move_yawrate = 0;		// solution [rad/s]
+  double R_norm = 0;        // solution error
+  double move_vel_x = 0;      // solution [m/s]
+  double move_vel_y = 0;      // solution [m/s]
+  double move_yawrate = 0;    // solution [rad/s]
 
-	VelocitySolver(int num_wheels_)
-		:	num_wheels(num_wheels_),
-			M(num_wheels_ * 2)
-	{
-		R.resize(M, 1);
-		J.resize(M, 3);
-	}
+  explicit VelocitySolver(int num_wheels_)
+  : num_wheels(num_wheels_),
+    M(num_wheels_ * 2)
+  {
+    R.resize(M, 1);
+    J.resize(M, 3);
+  }
 
-	void solve(const std::vector<OmniWheel>& wheels)
-	{
-		if((int)wheels.size() != num_wheels) {
-			throw std::logic_error("(int)wheels.size() != num_wheels");
-		}
+  void solve(const std::vector < OmniWheel > & wheels)
+  {
+    if ((int)wheels.size() != num_wheels) {
+      throw std::logic_error("(int)wheels.size() != num_wheels");
+    }
 
-		// make two iterations to get final R_norm
-		for(int iter = 0; iter < 2; ++iter)
-		{
-			J.fill(0);		// unset J values should be zero
+    // make two iterations to get final R_norm
+    for (int iter = 0; iter < 2; ++iter) {
+      J.fill(0);    // unset J values should be zero
 
-			for(int i = 0; i < num_wheels; ++i)
-			{
-				const double wheel_pos_radius = wheels[i].get_wheel_pos_radius();	// wheel position in polar coords [m]
-				const double wheel_pos_angle = wheels[i].get_wheel_pos_angle();		// wheel position in polar coords [rad]
-				const double wheel_angle = wheels[i].wheel_angle;
-				const double wheel_vel = wheels[i].wheel_vel;
+      for (int i = 0; i < num_wheels; ++i) {
+        // wheel position in polar coords [m]
+        const double wheel_pos_radius = wheels[i].get_wheel_pos_radius();
+        // wheel position in polar coords [rad]
+        const double wheel_pos_angle = wheels[i].get_wheel_pos_angle();
+        const double wheel_angle = wheels[i].wheel_angle;
+        const double wheel_vel = wheels[i].wheel_vel;
 
-				R[i * 2 + 0] = move_vel_x-wheel_vel*cos(wheel_angle)-wheel_pos_radius*sin(wheel_pos_angle)*move_yawrate;
-				R[i * 2 + 1] = -wheel_vel*sin(wheel_angle)+wheel_pos_radius*cos(wheel_pos_angle)*move_yawrate+move_vel_y;
+        R[i * 2 + 0] = move_vel_x - wheel_vel * cos(wheel_angle) - wheel_pos_radius * sin(
+          wheel_pos_angle) * move_yawrate;
+        R[i * 2 + 1] = -wheel_vel * sin(wheel_angle) + wheel_pos_radius * cos(wheel_pos_angle) *
+          move_yawrate + move_vel_y;
 
-				J(i * 2 + 0, 0) = 1;
-				J(i * 2 + 1, 1) = 1;
-				J(i * 2 + 0, 2) = -wheel_pos_radius*sin(wheel_pos_angle);
-				J(i * 2 + 1, 2) = wheel_pos_radius*cos(wheel_pos_angle);
-			}
-			R_norm = R.norm();
+        J(i * 2 + 0, 0) = 1;
+        J(i * 2 + 1, 1) = 1;
+        J(i * 2 + 0, 2) = -wheel_pos_radius * sin(wheel_pos_angle);
+        J(i * 2 + 1, 2) = wheel_pos_radius * cos(wheel_pos_angle);
+      }
+      R_norm = R.norm();
 
-			// solve Gauss-Newton step
-			const Matrix<double, 3, 3> H(J.transpose() * J);
-			const Matrix<double, 3, 1> X = H.inverse() * Matrix<double, 3, 1>(J.transpose() * R);
+      // solve Gauss-Newton step
+      const Matrix < double, 3, 3 > H(J.transpose() * J);
+      const Matrix < double, 3, 1 > X = H.inverse() * Matrix < double, 3, 1 > (J.transpose() * R);
 
-			move_vel_x -= X[0];
-			move_vel_y -= X[1];
-			move_yawrate -= X[2];
-		}
-	}
+      move_vel_x -= X[0];
+      move_vel_y -= X[1];
+      move_yawrate -= X[2];
+    }
+  }
 
 private:
-	const int num_wheels = 0;
-	const int M = 0;
+  const int num_wheels = 0;
+  const int M = 0;
 
-	MatrixX<double> R;			// residual vector
-	MatrixX<double> J;			// jacobian matrix
-
+  MatrixX < double > R;      // residual vector
+  MatrixX < double > J;      // jacobian matrix
 };
 
-
-#endif // VELOCITYSOLVER_H_
+#endif  // VELOCITYSOLVER_H_
