72d87d1420d551cbcfc22f98c1182c688760e68c
Pradheep <padhupradheep@gmail.com>
2025-05-30 18:03:55 +0200

    adding a namespacing to the joint states of the drives - fixes delta time error issue

--- src/neo_omnidrive_node.cpp
+++ src/neo_omnidrive_node.cpp
@@ -157,7 +157,7 @@ public:
       std::bind(&NeoOmniDriveNode::cmd_vel_callback, this, _1));
     m_sub_joint_state =
       this->create_subscription<sensor_msgs::msg::JointState>(
-      "joint_states", 1,
+      "drives/joint_states", 1,
       std::bind(&NeoOmniDriveNode::joint_state_callback, this, _1));
     m_sub_joy =
       this->create_subscription<sensor_msgs::msg::Joy>(
